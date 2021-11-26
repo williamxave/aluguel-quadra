f427ef262be97cc668abb90718a0a122245558c4
William Cristiano Bohn <xavegithub@gmail.com>
2021-11-25 21:25:43 -0300

    setup project

---
+++ src/main/java/com/projetoalguel/projetoAlguel/ProjetoAlguelApplication.java
@@ -0,0 +1,13 @@
+package com.projetoalguel.projetoAlguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAlguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAlguelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoalguel/projetoAlguel/ProjetoAlguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoalguel.projetoAlguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAlguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

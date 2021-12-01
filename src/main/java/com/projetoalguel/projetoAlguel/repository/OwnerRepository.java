ef1d9e90f511c0e1031720ba40c136c57ec589f7
William Cristiano Bohn <xavegithub@gmail.com>
2021-11-30 22:35:05 -0300

    Finished Owner crud

--- src/main/java/com/projetoalguel/projetoAlguel/repository/OwnerRepository.java
+++ src/main/java/com/projetoalguel/projetoAlguel/repository/OwnerRepository.java
@@ -4,6 +4,11 @@ import com.projetoalguel.projetoAlguel.domain.owner.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+    Optional<Owner> findOwnerByExternalId(UUID externalId);
 }
--- src/main/java/com/projetoalguel/projetoAlguel/service/OwnerService.java
+++ src/main/java/com/projetoalguel/projetoAlguel/service/OwnerService.java
@@ -2,12 +2,14 @@ package com.projetoalguel.projetoAlguel.service;
 
 import com.projetoalguel.projetoAlguel.domain.owner.Owner;
 import com.projetoalguel.projetoAlguel.domain.owner.dto.OwnerDto;
+import com.projetoalguel.projetoAlguel.domain.owner.dto.OwnerResponse;
 import com.projetoalguel.projetoAlguel.mapper.MapperInterface;
 import com.projetoalguel.projetoAlguel.repository.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class OwnerService {
@@ -23,11 +25,37 @@ public class OwnerService {
         this.mapperInterface = mapperInterface;
     }
 
-    public Owner toOwner(OwnerDto ownerDto){
-       var owner = mapperInterface.ownerDtoFromOwner(ownerDto);
+    public Owner toOwner(OwnerDto ownerDto) {
+        var owner = mapperInterface.ownerDtoFromOwner(ownerDto);
+        ownerRepository.save(owner);
+        return owner;
+    }
 
-        System.out.println(owner.getId());
-       ownerRepository.save(owner);
-       return owner;
+    public Optional<Owner> deleteOnwer(String externalId) {
+        var possibleOwner = searchOwner(externalId);
+        if (possibleOwner.isEmpty()) {
+            return Optional.empty();
+        }
+        possibleOwner.ifPresent(owner -> ownerRepository.delete(owner));
+        return possibleOwner;
+    }
+
+    public Optional<Owner> searchOwner(String externalId) {
+        var possibleOwner = ownerRepository.findOwnerByExternalId(UUID.fromString(externalId))
+                .orElseThrow(() -> new IllegalArgumentException("Owner not found"));
+        return Optional.ofNullable(possibleOwner);
+    }
+
+    public OwnerResponse ownerFromOwnerResponse(String externalId){
+        return searchOwner(externalId).map(owner -> new OwnerResponse(owner)).get();
+    }
+
+    public void updateOwner (String externalId, OwnerDto ownerDto){
+        var owner = searchOwner(externalId);
+        owner.get().setEmail(ownerDto.getEmail());
+        owner.get().setName(ownerDto.getName());
+        owner.get().setPassword(ownerDto.getPassword());
+        owner.get().upDate();
+        ownerRepository.save(owner.get());
     }
 }
